50a22c16eb1bf59cb64cdd34af297ac2f5ef19c3
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-04 17:59:33 -0700

    10 files at root
    The current 10 files at the root of the project.

---
+++ JavaScript Test references/FOOTER_JS_2019_EDITION_1.js
@@ -0,0 +1,3 @@
+// footer version: 2019.0 (Build 1)
+System.out.println ("Copyleft (c) Sean Walla Walla 2019 ");
+// This footer is really helpful, as now I only need to call 1 file per page and not retype it everywhere
